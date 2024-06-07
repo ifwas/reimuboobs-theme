788cb177a9f343884bf82d5a4f7ac6ab61779946
ifwas <ifwaslvl363@gmail.com>
2024-06-06 19:28:19 -0700

    fucked up
    the volume callbacinput fucked up real bad that not even i was able to enter songs sometimes

--- BGAnimations/_mousewheelscroll.lua
+++ BGAnimations/_mousewheelscroll.lua
@@ -38,44 +38,11 @@ local function scrollInput(event)
 	return false
 end
 
-function volControlBind(event)
-    local AltPressed = INPUTFILTER:IsBeingPressed("left alt")
- 
-
-    --mouse
-    if AltPressed and event.DeviceInput.button == "DeviceButton_mousewheel up" then
-        curGameVolume = clamp(curGameVolume + 0.05, 0, 1)
-        SOUND:SetVolume(curGameVolume)
-        MESSAGEMAN:Broadcast("volumeChanged")
-
-    end
-    if AltPressed and event.DeviceInput.button == "DeviceButton_mousewheel down" then
-        curGameVolume = clamp(curGameVolume - 0.05, 0, 1)
-        SOUND:SetVolume(curGameVolume)
-        MESSAGEMAN:Broadcast("volumeChanged")
-    end
-    --kb
-    --this doesn't ignore other actions that depend on arrow keys so i'll just leave this out
-    -- if AltPressed and event.DeviceInput.button == "DeviceButton_up" then
-    --     curGameVolume = clamp(curGameVolume + 0.025, 0, 1)
-    --     SOUND:SetVolume(curGameVolume)
-    -- end
-    -- if AltPressed and event.DeviceInput.button == "DeviceButton_down" then
-    --     curGameVolume = clamp(curGameVolume - 0.025, 0, 1)
-    --     SOUND:SetVolume(curGameVolume)
-    -- end
-end
-
---you broke it retard, congrats
 local t = Def.ActorFrame {
 	BeginCommand = function(self)
 		whee = SCREENMAN:GetTopScreen():GetMusicWheel()
 		top = SCREENMAN:GetTopScreen()
-		if event.DeviceInput.button ==  "left alt" then
-		top:AddInputCallback(volControlBind)
-		else
 		top:AddInputCallback(scrollInput)
-		end
 		self:visible(false)
 
 	end
